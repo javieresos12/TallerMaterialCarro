fe0b7417c082f54d29673fdb742b20562e1fc989
javier <javieresos12@gmail.com>
2018-10-04 21:29:44 -0500

    aplicacion finalizada

---
+++ app/src/main/java/com/example/android/tallercarro/AdaptadorCarro.java
@@ -0,0 +1,73 @@
+package com.example.android.tallercarro;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AdaptadorCarro  extends
+        RecyclerView.Adapter<AdaptadorCarro.CarrosViewHolder> {
+    private ArrayList<Carro> carros;
+    private Context contexto;
+
+
+    public AdaptadorCarro(ArrayList<Carro> carros, Context contexto) {
+        this.carros = carros;
+        this.contexto = contexto;
+    }
+
+    @Override
+    public CarrosViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.itemcarro,parent,false);
+        return new CarrosViewHolder(v);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(CarrosViewHolder holder, int position) {
+        Carro c = carros.get(position);
+        holder.foto.setImageResource(c.getFoto());
+        holder.placa.setText(c.getPlaca());
+        holder.precio.setText(""+c.getPrecio());
+        holder.color.setText(contexto.getResources().getStringArray(R.array.color)[c.getColor()]);
+        holder.marca.setText(contexto.getResources().getStringArray(R.array.marca)[c.getMarca()]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return carros.size();
+    }
+
+    public static class CarrosViewHolder extends RecyclerView.ViewHolder{
+        private ImageView foto;
+        private TextView placa;
+        private EditText precio;
+        private TextView color;
+        private TextView marca;
+        private TextView apellido;
+        private View v;
+
+        public CarrosViewHolder(View itemView){
+            super(itemView);
+            v = itemView;
+            foto = v.findViewById(R.id.fotos);
+            placa = v.findViewById(R.id.txtPlaca);
+            precio = v.findViewById(R.id.txtPrecio);
+            color= v.findViewById(R.id.cmbColor);
+            marca= v.findViewById(R.id.cmbMarca);
+
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/android/tallercarro/Principal.java
+++ app/src/main/java/com/example/android/tallercarro/Principal.java
@@ -1,15 +1,24 @@
 package com.example.android.tallercarro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class Principal extends AppCompatActivity {
+    private Intent i;
+    private RecyclerView lstOpciones;
+    private ArrayList<Carro> carros;
+    private LinearLayoutManager llm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,16 @@ public class Principal extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        lstOpciones = findViewById(R.id.lstCarro);
+        carros = Datos.obtener();
+
+        AdaptadorCarro adaptadorcarro = new AdaptadorCarro(carros, this);
+
+        llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        lstOpciones.setLayoutManager(llm);
+        lstOpciones.setAdapter(adaptadorcarro);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,25 +47,12 @@ public class Principal extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_principal, menu);
-        return true;
+    public void agregarCarros(View v){
+        i = new Intent(Principal.this,AgregarCarrros.class);
+        startActivity(i);
+        finish();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
